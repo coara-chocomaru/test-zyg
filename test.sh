144f66586fc95942cd07a1c864bfa586340574ac
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-26 18:41:55 +0900

    Update base64 encoded command in test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set +e
 export TMPDIR=/cache/
 
 base64 -d > /cache/command << 'B64EOF'
-Ym9vdC1yZWNvdmVyeQotLXNpZGVsb2FkCi0tcmV0cnlfY291bnQ9Mwo=
+Ym9vdC1yZWNvdmVyeQotLWFkYmQKLS1yZXRyeV9jb3VudD0zCg==
 B64EOF
