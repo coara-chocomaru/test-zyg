a69d9cc0889273ebf93a2e6d85a16880f26ce856
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-29 11:15:25 +0900

    Modify base64 encoded commands in test.sh
    Updated base64 encoded content in test.sh.

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ set +e
 #test
 export TMPDIR=/cache/
 base64 -d > /cache/command << 'B64EOF'
-Ym9vdC1yZWNvdmVyeQotLWJvb3Rsb2FkZXIKLS1ib290LWJvb3Rsb2FkZXIKLS1ib290b25jZS1ib290bG9hZGVyCi0tZmFzdGJvb3RfcGxlYXNlCi0tcmV0cnlfY291bnQ9Mwpib290bG9hZGVyCmJvb3QtYm9vdGxvYWRlcgpib290b25jZS1ib290bG9hZGVyCnJldHJ5X2NvdW50PTMK
+Ym9vdC1yZWNvdmVyeQotLXNpZGVsb2FkCi0tYWRiZAo=
 B64EOF
